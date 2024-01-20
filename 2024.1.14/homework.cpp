e890c201f5e8cde24480e38fb3f9df9a1dad2bc6
Eason20120229 <181348642@qq.com>
2024-01-20 17:01:01 +0800

    "Commit files"

--- 2024.1.14/homework.cpp
+++ 2024.1.14/homework.cpp
@@ -12,5 +12,14 @@ int main(){
     for(int i = 0;i < n;i++){
         cin >> data[i].w >> data[i].n;
     }
+    int t = 0;
+    for(int i = 0;i < m;i++){
+        int w,s;
+        cin >> w >> s;
+        if(data[t].w == 0) w = (w + 1) % 2;
+        if(w == 1) t = (t - s + n) % n;
+        if(w == 0) t = (t + s) % n;
+    }
+    cout << data[t].n;
     return 0;
 }
